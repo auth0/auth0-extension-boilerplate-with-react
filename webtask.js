fb8334cacb9962b595f3de8c2541efacb3895337
Saltuk Alakus <saltukalakus@gmail.com>
2016-08-22 01:45:56 +0300

    Webtask context secrets are managed both for deploy and local test modes.

--- webtask.js
+++ webtask.js
@@ -1,5 +1,22 @@
+var nconf = require('nconf');
 var Webtask = require('webtask-tools');
 
-// This is the entry-point for the Webpack build. We need to convert our module
-// (which is a simple Express server) into a Webtask-compatible function.
-module.exports = Webtask.fromExpress(require('./index.js'));
+var server = null;
+const getServer = function(req, res){
+  if (!server) {
+    nconf.defaults({
+      AUTH0_DOMAIN: req.webtaskContext.secrets.AUTH0_DOMAIN,
+      EXTENSION_SECRET: req.webtaskContext.secrets.EXTENSION_SECRET,
+      WT_URL: req.webtaskContext.secrets.WT_URL,
+      NODE_ENV: 'development',
+      HOSTING_ENV: 'webtask',
+      CLIENT_VERSION: process.env.CLIENT_VERSION
+    });
+
+    // Start the server.
+    server = require('./index.js');
+  }
+  return server(req, res);
+};
+
+module.exports = Webtask.fromExpress(function(req, res){return getServer(req, res)});
